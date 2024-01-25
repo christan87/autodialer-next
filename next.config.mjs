90d1ec0a65e71e5b488200560442d5f2b3de9f5b
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2024-01-25 10:58:42 -0800

    added .env.local for next.js environment and .env for node. Successfully connecting to mongodb and socket io

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  env: {
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
+  }
+}
 
 export default nextConfig;
--- server/app.js
+++ server/app.js
@@ -1,5 +1,27 @@
+require('dotenv').config();
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const mongoConnectionString = process.env.DB_CONNECTION_STRING;
+
+// Connect to MongoDB
+mongoose.connect(`${mongoConnectionString}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 // Define your custom routes here
 app.get('/api/data', (req, res) => {
@@ -9,3 +31,4 @@ app.get('/api/data', (req, res) => {
 });
 
 module.exports = app;
+
